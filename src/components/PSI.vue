30e1775bf21e982118ce0f628a384d769a6d93ab
Arturo <arubio@seocom.agency>
2020-11-14 16:46:39 +0100

    core web vitals info

--- src/components/PSI.vue
+++ src/components/PSI.vue
@@ -1,54 +1,68 @@
 <template>
-	<div class="psi">
-		<div id="PSI-info">
-			<p>Web: {{ infoFromTest.id }}</p>
-			<p>LARGEST_CONTENTFUL_PAINT_MS:
-				{{ infoFromTest.loadingExperience }}</p>
-		</div>
-		<div class="h-100"></div>
-	</div>
+  <div class="psi">
+    <div id="PSI-info" v-if="infoFromTest">
+      <p>Web: {{ infoFromTest.id }}</p>
+      <p>
+        CLS:
+        {{
+          infoFromTest.lighthouseResult.audits.metrics.details.items[0].cumulativeLayoutShift
+        }}
+      </p>
+      <p>
+        LCP:
+        {{
+          infoFromTest.lighthouseResult.audits.metrics.details.items[0].largestContentfulPaint
+        }}
+      </p>
+      <p>
+        FID:
+        {{
+          infoFromTest.lighthouseResult.audits.metrics.details.items[0].maxPotentialFID
+        }} ms
+      </p>
+      <div class="h-100"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-	import axios from 'axios'
-  
-	export default {
-    name: 'psi',
-    data() {
-      return {
-        infoFromTest: {}
-      }
-    },
-    created() {
-      const psiApiUrl = "https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://seocom.agency";
+import axios from "axios";
 
-      axios.get(psiApiUrl)
-        .then(response => 
-          this.infoFromTest = response.data
-        );
-    }
+export default {
+  name: "psi",
+  data() {
+    return {
+      infoFromTest: null
+    };
+  },
+  created() {
+    const psiApiUrl =
+      "https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://seocom.agency";
+
+    axios.get(psiApiUrl).then(response => (this.infoFromTest = response.data));
   }
+};
 </script>
 
 <style scoped>
-	#PSI-info {
-		text-align: left;
-	}
+#PSI-info {
+  text-align: left;
+}
 
-	ul {
-		list-style-type: none;
-		padding: 0;
-	}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-	li {
-		text-align: left !important;
-	}
+li {
+  text-align: left !important;
+}
 
-	a {
-		color: #42b983;
-	}
+a {
+  color: #42b983;
+}
 
-	span {
-		color: red;
-	}
+span {
+  color: red;
+}
 </style>
